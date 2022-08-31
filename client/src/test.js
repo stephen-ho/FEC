6912c0b71ac857e869561ef9303f91da82d86cd2
Stephen Ho <wickedcz4a@gmail.com>
2022-08-30 17:19:10 -0700

    installed react-test-render

--- client/src/test.js
+++ client/src/test.js
@@ -1,4 +1,4 @@
-const questions = require('./components/QuestionList.jsx');
+const questions = require('./components/QuestionList');
 
 test('renders an array of questions', () => {
   expect(questions.toBeDefined());
